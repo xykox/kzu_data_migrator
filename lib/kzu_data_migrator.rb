bc87a46a6fc9ac33eee2fac04ef40f56945f5e6d
Jean Marc Delafont <jmdelafont@xykox.net>
2016-03-24 16:05:07 +0100

    missing require

--- lib/kzu_data_migrator.rb
+++ lib/kzu_data_migrator.rb
@@ -1,5 +1,8 @@
 require 'kzu_data_migrator/version'
 
+require 'kzu_data_migrator/migration'
+require 'kzu_data_migrator/data_migration_version'
+require 'kzu_data_migrator/migrator'
 require 'kzu_data_migrator/railtie'
 
 module KzuDataMigrator
